556c7886d6c7d4db560a2b1e7948795b44d00b0a
flyether <flyether@gmail.com>
2022-04-27 22:01:12 +0300

    feat: add caps

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function transform(arr) {
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === null ||arr[i] == undefined || typeof arr[i] !== 'string' ||arr[i] === false) {
+      throw new NotImplementedError();
+      }
+      if (arr[i] === '--discard-next') {
+          let result = arr.slice(0, i) + "," + arr.slice(i + 2);
+          let newArray = result;
+          return newArray;
+      } else if (arr[i] === '--discard-prev') {
+          let result = arr.slice(0, i - 1) + "," + arr.slice(i + 1);
+          let newArray = result;
+          return newArray;
+      } else if (arr[i] === '--double-next') {
+          let result = arr.slice(0, i) + "," + arr[i + 1] + "," + arr.slice(i + 1);
+          let newArray = result;
+          return newArray;
+      } else if (arr[i] === '--double-prev') {
+          let result = arr.slice(0, i) + "," + arr[i - 1] + "," + arr.slice(i + 1);
+          let newArray = result;
+      
+          return newArray;
+          
+      }
+  }
+};
+
 
 module.exports = {
   transform
