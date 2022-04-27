6e2db0fd812a028de6ec523b1fd48345e84cb053
flyether <flyether@gmail.com>
2022-04-27 22:35:04 +0300

    feat: add caps

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,10 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount( inStr, cStr) {
+  var result = inStr.split('').filter(function(letter) {
+    return (cStr.indexOf(letter) > -1);
+  });
+  return result;
 }
+
+// for (i = 0; i < (arr.length-1); i++) { /* перебираем циклом строки из массива */
+//   var result = (i == 0) ? compare(arr[i], arr[i+1]) : compare(result.join(''), arr[i+1]);
+
+
 
 module.exports = {
   getCommonCharacterCount
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,8 +6,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 const chainMaker = {
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+   
+   
   },
   addLink(/* value */) {
     throw new NotImplementedError('Not implemented');
